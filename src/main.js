017dd68d3a90d946538f5a0d44474c0fd9aeed31
vee <phanveehuen@gmail.com>
2023-02-21 12:12:11 +0800

    Initial commit - starter template with navbar and router

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import {createApp} from 'vue';
+import {Quasar} from 'quasar';
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css';
+
+// Import Quasar css
+import 'quasar/src/css/index.sass';
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
+import App from './App.vue';
+import router from "./router/index.js";
+
+const myApp = createApp(App);
+
+myApp.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+});
+
+myApp.use(router);
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app');
